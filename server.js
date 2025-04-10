007037c07995c420bb4b7fa5ad38bbabb8136a59
Developer-GoQuestMedia <vivekkumar.singh@goquestmedia.com>
2025-04-10 13:07:55 +0530

    Assignment

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const { addDays, addWeeks, subDays, format } = require('date-fns');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+  const today = new Date();
+
+  const response = {
+    'add_6_days': format(addDays(today, 6), 'dd-MMM-yyyy'),
+    'add_6_weeks': format(addWeeks(today, 6), 'dd-MMM-yyyy'),
+    'subtract_187_days_from_12-Jan-2019': format(subDays(new Date('2019-01-12'), 187), 'dd-MMM-yyyy')
+  };
+
+  res.end(JSON.stringify(response));
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
